bc83379c24fe0f06b7ef383697c6dd76078ed429
Helena Jakobsen <helenajakobsen@helenas-air-4.lan>
2021-02-18 15:36:04 +0100

    indsættelse af loop og single view

--- detalje.js
+++ detalje.js
@@ -1,17 +1,14 @@
-// AL NEDENSTÅENDE KODE ER KOPIERET FRA KAMILLES BABUSHKA-OPGAVE, SÅ NAVNE, LINKS, ID'ER OSV SKAL RETTES TIL
-
-
 //Disse konstanter henter URL-parametrene fra vores index.html. Det URL-parameter vi gerne vil have med er ID'et fra vores objekt, hvilket er det vi får ved at sætte nedenstående variabler
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
 //Her sætter vi globale konstante variabler, der hiver fat i vores genererede data fra restdb
-const medieurl = "https://babushka-dd8a.restdb.io/media/";
-let ret;
+const medieurl = "https://ruter-80b2.restdb.io/media/";
+let rute;
 
 //Denne API-key giver os adgang til vores data i vores restdb
 const myHeaders = {
-    "x-apikey": "600ec2fb1346a1524ff12de4"
+    "x-apikey": "602e59735ad3610fb5bb62e6"
 }
 console.log("ID", id);
 
@@ -20,25 +17,25 @@ document.addEventListener("DOMContentLoaded", loadJSON);
 
 
 //Her sætter vi en asynkron funktion der fortæller at vi gerne vil fetche dataen fra vores variabel "url". Vi fortæller funktionen at den skal vente med at oprette variablen "respons", til al fetch har hentet vores data
-//Variablen "retter" bruger vi til at lave vores data om til json. Derefter bliver funktionen visRuter kaldt
+//Variablen "ruter" bruger vi til at lave vores data om til json. Derefter bliver funktionen visRuter kaldt
 async function loadJSON() {
-    const JSONData = await fetch(`https://babushka-dd8a.restdb.io/rest/menu/${id}`, {
+    const JSONData = await fetch(`https://ruter-80b2.restdb.io/rest/ruteliste/${id}`, {
         headers: myHeaders
     });
-    ret = await JSONData.json();
+    rute = await JSONData.json();
 
-    console.log("Retter", ret);
-    visRet(ret);
+    console.log("Ruter", rute);
+    visRuter(rute);
 
 }
 
 //I denne funktion får vi vist id'et for det objekt vi har haft trykket på. Vi vil gerne have noget content der svarer til det objekt fra vores array der bliver vist. Al dette content er det vi har genereret på restdb, og hentet ind i vores dokument som json
 //Derudover har vi tilføjet en eventlistener på den tilbage-knap vi har implementeret på vores singleview, der gør at vores funktion tilbageMenu bliver kaldt, når man klikker på det
-function visRet() {
-    document.querySelector(".billede").src = medieurl + ret.billede;
-    document.querySelector(".ret").textContent = ret.navn;
-    document.querySelector(".beskrivelse").textContent = ret.langbeskrivelse;
-    document.querySelector(".pris").textContent = `Pris: ${ret.pris},-`;
+function visRuter() {
+    document.querySelector(".naturbillede").src = medieurl + rute.naturbillede;
+    document.querySelector(".rutenavn").textContent = rute.rutenavn;
+    document.querySelector(".område").textContent = rute.område;
+    document.querySelector(".distance").textContent = rute.distance;
 
     document.querySelector("a").addEventListener("click", tilbageMenu);
 }
--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// AL NEDENSTÅENDE KODE ER KOPIERET FRA KAMILLES BABUSHKA-OPGAVE, SÅ NAVNE, LINKS, ID'ER OSV SKAL RETTES TIL
-
-
-
 //Her sætter vi globale konstante variabler, der hiver fat i vores genererede data fra restdb
-const url = "https://babushka-dd8a.restdb.io/rest/menu";
-const medieurl = "https://babushka-dd8a.restdb.io/media/";
+const url = "https://ruter-80b2.restdb.io/rest/ruteliste";
+const medieurl = "https://ruter-80b2.restdb.io/media/";
 
 //Denne variabel hiver fat i vores h2'er i vores html-dokument, da vi skal kunne ændre textcontent når vi filtrere, for at fortælle brugeren hvilken knap der er blevet trykket på
+
 const header = document.querySelector("header h2");
+
+
 
 //Denne API-key giver os adgang til vores data i vores restdb
 const options = {
     headers: {
-        'x-apikey': "600ec2fb1346a1524ff12de4"
+        'x-apikey': "602e59735ad3610fb5bb62e6"
     }
 };
 
@@ -21,7 +20,7 @@ const options = {
 document.addEventListener("DOMContentLoaded", start)
 
 //Disse globale variabler er vigtige at få sat, så vi senere kan bruge dem
-let retter;
+let ruter;
 let filter = "alle";
 
 //Denne funktion bliver kaldt, når al vores DOM-content er loaded
@@ -29,11 +28,12 @@ let filter = "alle";
 //Derudover sætter vi en eventlistener på hver af disse knapper, da vi gerne vil kalde en funktion, når der klikkes på en af dem
 //Denne funktion kalder til sidst også en ny funktion, kaldet hentData, der sørger for at fetch'e al vores data fra restdb
 function start() {
-    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+    console.log("start");
+    //document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 
     const filterKnapper = document.querySelectorAll("nav a");
 
-    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerRetter));
+    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerRuter));
     hentData();
 }
 
@@ -44,9 +44,11 @@ function start() {
 //Til sidst tilføjer vi .valgt til den knap vi har trykket på. Denne klasse er stylet således, at den knap man har valgt, ændrer farve. Dette er for at fremme brugervenligheden
 //.valgt bliver derudover fjernet fra den knap der lige har haft den
 //Til sidst kalder denne funktion også en anden funktion kaldet visRuter, der gør at vores array med alle vores objekter bliver vist i et loop-view
-function filtrerRetter() {
-    filter = this.dataset.retter;
-    console.log("Retter", filter);
+
+
+function filtrerRuter() {
+    filter = this.dataset.ruter;
+    console.log("Ruter", filter);
     document.querySelector(".valgt").classList.remove("valgt");
     this.classList.add("valgt");
     header.textContent = this.textContent;
@@ -56,12 +58,12 @@ function filtrerRetter() {
     visRuter();
 }
 
-//Her sætter vi en asynkron funktion der fortæller at vi gerne vil fetche dataen fra vores variabel "url". Vi fortæller funktionen at den skal vente med at oprette variablen "respons", til al fetch har hentet vores data
-//Variablen "retter" bruger vi til at lave vores data om til json. Derefter bliver funktionen visRuter kaldt
+//Her sætter vi en asynkron funktion der fortæller at vi gerne vil fetche dataen fra vores variabel "url". Vi fortæller funktionen at den skal vente med at opruter variablen "respons", til al fetch har hentet vores data
+//Variablen "ruter" bruger vi til at lave vores data om til json. Derefter bliver funktionen visRuter kaldt
 async function hentData() {
     const respons = await fetch(url, options);
-    retter = await respons.json();
-    console.log("Retter", retter);
+    ruter = await respons.json();
+    console.log("Ruter", ruter);
     visRuter();
 }
 
@@ -73,15 +75,16 @@ function visRuter() {
     const template = document.querySelector("template").content;
     main.textContent = "";
 
-    retter.forEach(ret => {
-        console.log("Retter");
-        if (filter == ret.kategori || filter == "alle") {
+    ruter.forEach(rute => {
+        console.log("Rute");
+        if (filter == rute.kategori || filter == "alle") {
             const klon = template.cloneNode(true);
-            klon.querySelector(".billede").src = medieurl + ret.billede;
-            klon.querySelector(".ret").textContent = ret.navn;
-            klon.querySelector(".beskrivelse").textContent = ret.kortbeskrivelse;
-            klon.querySelector(".pris").textContent = `Pris: ${ret.pris},-`;
-            klon.querySelector("article").addEventListener("click", () => visDetaljer(ret));
+            klon.querySelector(".naturbillede").src = medieurl + rute.naturbillede;
+            klon.querySelector(".rutenavn").textContent = rute.rutenavn;
+            klon.querySelector(".område").textContent = rute.område;
+            klon.querySelector(".distance").textContent = rute.distance;
+
+            klon.querySelector("article").addEventListener("click", () => visDetaljer(ruter));
             main.appendChild(klon);
         }
     })
@@ -90,8 +93,9 @@ function visRuter() {
 
 //Denne funktion tager fat i id'et for præcis det objekt vi har klikket på, og kan, ved hjælp af html-siden til vores singleview, vise al den rigtige content for dette objekt
 function visDetaljer(hvad) {
-    location.href = `02-detalje.html?id=${hvad._id}`;
+    location.href = `detalje.html?id=${hvad._id}`;
 }
+
 
 function toggleMenu() {
     console.log("toggleMenu");
@@ -105,7 +109,5 @@ function toggleMenu() {
     } else {
         document.querySelector("#menuknap").textContent = "X";
     }
-
-
 
 }
